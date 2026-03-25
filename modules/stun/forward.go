2a1a4ecde4d25e751bb07a74125555447b55431e
ZluxYao <zluxyao@gmail.com>
2026-03-25 09:35:26 +0800

    修复Forward 泄漏

--- modules/stun/forward.go
+++ modules/stun/forward.go
@@ -14,13 +14,16 @@ func Forward(src net.Conn, targetAddr string, protocol string) {
 
 	dst, err := net.DialTimeout(protocol, targetAddr, 3*time.Second)
 	if err != nil {
-		logrus.Errorf("❌ 连接内网目标失败 [%s]: %v", targetAddr, err)
+		logrus.Errorf("连接内网目标失败 [%s]: %v", targetAddr, err)
 		return
 	}
 	defer dst.Close()
 
 	go func() {
 		_, _ = io.Copy(dst, src)
+		dst.Close() // src断了，关掉dst，让下面的io.Copy立刻返回
 	}()
+
 	_, _ = io.Copy(src, dst)
+	src.Close() // dst断了，关掉src，让上面的io.Copy立刻返回
 }
